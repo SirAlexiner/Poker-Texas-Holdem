90ac16e3a3c2c7e08ef7c1ddb9ae82c6104ab7ee
Torgrim Thorsen <torgrilt@stud.ntnu.no>
2023-03-17 20:17:43 +0100

    extended Main to Application and initiated a FX windows

--- src/main/java/no/ntnu/idatg2001/torgrilt/Main.java
+++ src/main/java/no/ntnu/idatg2001/torgrilt/Main.java
@@ -1,8 +1,36 @@
 package no.ntnu.idatg2001.torgrilt;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage stage){
+    stage.setFullScreenExitHint("");
+    stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+    stage.setFullScreen(true);
+    stage.setResizable(false);
+
+    stage.setTitle("Texas HoldEm Poker");
+
+    StackPane root = new StackPane();
+
+    Scene scene = new Scene(root);
+
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @Override
+  public void stop() {
+    System.exit(0);
+  }
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    launch(args);
   }
 }
